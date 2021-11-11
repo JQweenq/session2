424d7d795fcd47050bc5a9733316b5cbca9c1f06
Josty-Qweenq <josty.qweenq@gmail.com>
2021-11-11 16:10:49 +0700

    initial commit

---
+++ app/src/main/java/com/example/secondstage/AboutActivity.java
@@ -0,0 +1,22 @@
+package com.example.secondstage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_aboute);
+    }
+    public void back(View view){
+    Intent i = new Intent(this, SignInActivity.class);
+    startActivity(i);
+    }
+}
