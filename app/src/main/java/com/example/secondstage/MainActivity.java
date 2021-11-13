d2edaf6b7b846871b5135fbe8e5d838ed6efea9d
Josty <Josty-Qweenq>
2021-11-14 01:12:28 +0700

    added method's body

--- app/src/main/java/com/example/secondstage/MainActivity.java
+++ app/src/main/java/com/example/secondstage/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.secondstage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void back(View view){
     /* kudato nazad... */
+        Intent i = new Intent(this, SignInActivity.class);
+        startActivity(i);
     }
 }
