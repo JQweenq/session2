48eb8c684efbc917a70a511fe43938c4570bd4d0
Josty <Josty-Qweenq>
2021-11-14 01:13:06 +0700

    added redirects to main activity

--- app/src/main/java/com/example/secondstage/SignInActivity.java
+++ app/src/main/java/com/example/secondstage/SignInActivity.java
@@ -2,7 +2,9 @@ package com.example.secondstage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class SignInActivity extends AppCompatActivity {
@@ -12,5 +14,14 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_in);
+    }
+    public void comeAsUser(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void comeAsGuest(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
